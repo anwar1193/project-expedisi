593981026e1391b58d03be770f58736355a48379
Heru Anggara <heruanggara0909@gmail.com>
2024-05-02 13:25:04 +0700

    feat: create view crud daftar pengeluaran

---
+++ resources/views/daftar-pengeluaran/edit.blade.php
@@ -0,0 +1,181 @@
+@extends('layouts.admin.master')
+
+@section('title')Edit Daftar Pengeluaran
+ {{ $title }}
+@endsection
+
+@push('css')
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/select2.css') }}">
+@endpush
+
+@section('content')
+	@component('components.breadcrumb')
+		@slot('breadcrumb_title')
+			<h3>Daftar Pengeluaram</h3>
+		@endslot
+		<li class="breadcrumb-item active"><a href="{{ route('daftar-pengeluaran') }}">Daftar Pengeluaran</a></li>
+        <li class="breadcrumb-item active">Edit</li>
+	@endcomponent
+	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="card">
+					<div class="card-header pb-0">
+						<h5>Form Edit Daftar Pengeluaran</h5>
+					</div>
+					<form class="form theme-form" method="POST" action="{{ route('daftar-pengeluaran.update', $datas->id) }}" enctype="multipart/form-data">
+                        @csrf
+						<div class="card-body">
+
+							<div class="row">
+								<div class="col">
+									<div class="mb-3">
+										<label class="form-label" for="">Tanggal Pengeluaran</label>
+										<input class="form-control @error('tgl_pengeluaran') is-invalid @enderror" type="date" name="tgl_pengeluaran" autocomplete="off" value="{{ old('tgl_pengeluaran', $datas->tgl_pengeluaran) }}"/>
+
+										@error('tgl_pengeluaran')
+										<div class="text-danger">
+											{{ $message }}
+										</div>
+										@enderror
+									</div>
+								</div>
+							</div>
+							
+                            <div class="row">
+								<div class="col">
+									<div class="mb-3">
+										<label class="form-label" for="">Keterangan</label>
+										<input class="form-control @error('keterangan') is-invalid @enderror" type="text" name="keterangan" autocomplete="off" value="{{ old('keterangan', $datas->keterangan) }}"/>
+
+										@error('keterangan')
+										<div class="text-danger">
+											{{ $message }}
+										</div>
+										@enderror
+									</div>
+								</div>
+							</div>
+                            
+                            <div class="row">
+								<div class="col">
+									<div class="mb-3">
+										<label class="form-label" for="">Jumlah Pembayaran</label>
+										<input class="form-control @error('jumlah_pembayaran') is-invalid @enderror" type="number" name="jumlah_pembayaran" autocomplete="off" value="{{ old('jumlah_pembayaran', $datas->jumlah_pembayaran) }}"/>
+
+										@error('jumlah_pembayaran')
+										<div class="text-danger">
+											{{ $message }}
+										</div>
+										@enderror
+									</div>
+								</div>
+							</div>
+                            
+                            <div class="row">
+								<div class="col">
+									<div class="mb-3">
+										<label class="form-label" for="">Pengguna Terkait</label>
+										<input class="form-control @error('pengguna_terkait') is-invalid @enderror" type="text" name="pengguna_terkait" autocomplete="off" value="{{ old('pengguna_terkait', $datas->pengguna_terkait) }}"/>
+
+										@error('pengguna_terkait')
+										<div class="text-danger">
+											{{ $message }}
+										</div>
+										@enderror
+									</div>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col">
+									<div class="mb-3">
+										<label class="form-label" for="">Metode Pembayaran</label>
+                                        <select name="metode_pembayaran" id="metode_pembayaran" class="form-control @error('metode_pembayaran') is-invalid @enderror">
+											<option value="tunai" {{ $datas->metode_pembayaran == 'tunai' ? 'selected' : NULL }}>Tunai</option>
+											<option value="transfer" {{ $datas->metode_pembayaran == 'transfer' ? 'selected' : NULL }}>Transfer</option>
+										</select>
+
+										@error('metode_pembayaran')
+										<div class="text-danger">
+											{{ $message }}
+										</div>
+										@enderror
+									</div>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col">
+									<div class="mb-3">
+                                        <div class="mb-3">
+                                            <label class="form-label" for="">Bukti Pembayaran</label>
+                                            <input class="form-control @error('bukti_pembayaran') is-invalid @enderror" type="file" width="48" height="48" name="bukti_pembayaran" />
+    
+                                            @error('bukti_pembayaran')
+                                            <div class="text-danger">
+                                                {{ $message }}
+                                            </div>
+                                            @enderror
+    
+                                            <img src="{{ asset('storage/daftar-pengeluaran/'.$datas->bukti_pembayaran) }}" alt="" width="200px" class="img-fluid mt-2">
+                                        </div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col">
+									<div class="mb-3">
+										<label class="form-label" for="">Status Pengeluaran</label>
+										<select name="status_pengeluaran" id="status_pengeluaran" class="form-control @error('status_pengeluaran') is-invalid @enderror">
+											<option value="1" {{ $datas->status_pembayaran == 1 ? 'selected' : NULL }}>Lunas</option>
+											<option value="2" {{ $datas->status_pembayaran == 2 ? 'selected' : NULL }}>Pending</option>
+										</select>
+
+										@error('status_pembayaran')
+										<div class="text-danger">
+											{{ $message }}
+										</div>
+										@enderror
+									</div>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col">
+									<div class="mb-3">
+										<label class="form-label" for="">Jenis Pengeluaran</label>
+                                        <select name="jenis_pengeluaran" id="jenis_pengeluaran" class="form-control @error('jenis_pengeluaran') is-invalid @enderror">
+											<option value="operasional" {{ $datas->jenis_pengeluaran == 'operasional' ? 'selected' : NULL }}>Operasional</option>
+											<option value="pengeluaran lain" {{ $datas->jenis_pengeluaran == 'pengeluaran lain' ? 'selected' : NULL }}>Pengeluaran Lain</option>
+										</select>
+
+										@error('jenis_pengeluaran')
+										<div class="text-danger">
+											{{ $message }}
+										</div>
+										@enderror
+									</div>
+								</div>
+							</div>
+
+						</div>
+						<div class="card-footer text-end">
+							<button class="btn btn-primary" type="submit">Simpan Data</button>
+							<a href="{{ route('data-pengiriman') }}" class="btn btn-light">Kembali</a>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	@push('scripts')
+    <script src="{{ asset('assets/js/select2/select2.full.min.js') }}"></script>
+    <script src="{{ asset('assets/js/select2/select2-custom.js') }}"></script>
+	@endpush
+
+@endsection
---
+++ resources/views/daftar-pengeluaran/index.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.admin.master')
+
+@section('title')Daftar Pengeluaran
+ {{ $title }}
+@endsection
+
+@push('css')
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/datatables.css') }}">
+@endpush
+
+@section('content')
+	@component('components.breadcrumb')
+		@slot('breadcrumb_title')
+			<h3>Daftar Pengeluaran</h3>
+		@endslot
+		<li class="breadcrumb-item active"><a href="{{ route('daftar-pengeluaran') }}">Daftar Pengeluaran</a></li>
+		<li class="breadcrumb-item active">Table</li>
+	@endcomponent
+
+    <nav class="page-breadcrumb">
+        <ol class="breadcrumb align-items-center">
+            <div class="d-grid gap-2 d-md-block mx-2">
+                {{-- @if (isAdmin()) --}}
+                    <a href="{{ route('daftar-pengeluaran.create') }}" class="btn btn-info" data-bs-toggle="tooltip" data-bs-placement="top" title="Tambah Data">
+                        <i class="fa fa-plus"></i> Tambah
+                    </a>
+                {{-- @endif --}}
+            </div>
+        </ol>
+    </nav>
+	
+	<div class="container-fluid">
+        <div class="row">
+        </div>
+	    <div class="row">
+	        <!-- Server Side Processing start-->
+	        <div class="col-sm-12">
+	            <div class="card">
+	                <div class="card-body">
+
+						@if (session()->has('success'))
+							<div class="alert alert-success alert-dismissible fade show" role="alert">
+								<strong>Berhasil <i class="fa fa-info-circle"></i></strong> 
+								{{ session('success') }}
+								<button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+							</div>
+						@endif
+
+						@if (session()->has('delete'))
+							<div class="alert alert-danger alert-dismissible fade show" role="alert">
+								<strong>Berhasil <i class="fa fa-info-circle"></i></strong> 
+								{{ session('delete') }}
+								<button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+							</div>
+						@endif
+						
+						@if (session()->has('error'))
+							<div class="alert alert-danger alert-dismissible fade show" role="alert">
+								<strong>Gagal <i class="fa fa-info-circle"></i></strong> 
+								{{ session('error') }}
+								<button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+							</div>
+						@endif
+	                    
+						{{-- Table --}}
+						<div class="table-responsive">
+	                        <table class="display" id="basic-1">
+	                            <thead>
+	                                <tr>
+	                                    <th>No</th>
+										<th>Tanggal Pengeluaran</th>
+										<th>Keterangan</th>
+	                                    <th>Jumlah Pembayaran</th>
+	                                    <th>Pengguna Terkait</th>
+	                                    <th>Status Pengeluaran</th>
+	                                    <th>Jenis Pengeluaran</th>
+										<th width="35%" class="text-center">Action</th>
+	                                </tr>
+	                            </thead>
+	                            <tbody>                                        
+                                    @foreach ($datas as $data)
+										<tr>
+											<td>{{ $loop->iteration; }}</td>
+											<td>{{ $data->tgl_pengeluaran }}</td>
+											<td>{{ $data->keterangan }}</td>
+											<td>{{ $data->jumlah_pembayaran }}</td>
+											<td>{{ $data->pengguna_terkait }}</td>
+											<td>{{ $data->status_pengeluaran == 1 ? 'Disetujui' : 'Pending'; }}</td>
+											<td>{{ $data->jenis_pengeluaran }}</td>
+											<td class="text-center">
+												<a class="btn btn-square btn-info btn-xs" data-bs-toggle="modal" data-original-title="test" data-bs-target="#modalDataPengiriman{{ $data->id }}"title="Detail Data">
+													<i class="fa fa-eye"></i>
+												</a>
+
+												<a href="{{ route('daftar-pengeluaran.edit', $data->id) }}" class="btn btn-square btn-warning btn-xs" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Data">
+													<i class="fa fa-edit"></i>
+												</a>
+												
+												<a href="{{ route('daftar-pengeluaran.delete', $data->id) }}" class="btn btn-square btn-danger btn-xs" data-bs-toggle="tooltip" data-bs-placement="top" title="Hapus Data" onclick="return confirm('Apakah Anda Yakin?')">
+													<i class="fa fa-trash"></i>
+												</a>
+												@include('daftar-pengeluaran.detail')
+											</td>
+										</tr>
+									@endforeach
+	                            </tbody>
+	                        </table>
+	                    </div>
+
+	                </div>
+	            </div>
+	        </div>
+	        <!-- Server Side Processing end-->
+	    </div>
+	</div>
+	
+	@push('scripts')
+	<script src="{{ asset('assets/js/datatable/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/js/datatable/datatables/datatable.custom.js') }}"></script>
+	@endpush
+
+@endsection
