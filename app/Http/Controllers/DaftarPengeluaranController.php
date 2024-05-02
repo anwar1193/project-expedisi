8c752f1c8598cd93ae746503a939c11dd0c67778
Heru Anggara <heruanggara0909@gmail.com>
2024-05-02 15:17:58 +0700

    feat: add method integarsi google drive api

--- app/Http/Controllers/DaftarPengeluaranController.php
+++ app/Http/Controllers/DaftarPengeluaranController.php
@@ -6,6 +6,8 @@ use App\Helpers\Helper;
 use App\Models\DaftarPengeluaran;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Yaza\LaravelGoogleDriveStorage\Gdrive;
 
 class DaftarPengeluaranController extends Controller
 {
@@ -38,15 +40,19 @@ class DaftarPengeluaranController extends Controller
 
         $foto = $request->file('bukti_pembayaran');
 
+        $namafile = 'daftar-pengeluaran/'.$foto->hashName();
+        $path = public_path('storage/daftar-pengeluaran/' . $foto->hashName());
+
         if($foto != ''){
             $foto->storeAs('public/daftar-pengeluaran', $foto->hashName());
+            Storage::disk('google')->put($namafile, File::get($path));
         }
 
         $validateData['bukti_pembayaran'] = ($foto != '' ? $foto->hashName() : '');
 
         DaftarPengeluaran::create($validateData);
 
-        Helper::logActivity('Simpan daftar pengeluaran dengan no resi : '.$request->no_resi);
+        Helper::logActivity('Simpan daftar pengeluaran');
 
         return redirect()->route('daftar-pengeluaran')->with('success', 'Data Berhasil Disimpan');
 
@@ -76,9 +82,15 @@ class DaftarPengeluaranController extends Controller
 
         $getImage = DaftarPengeluaran::find($id);
 
+        $namafile = 'daftar-pengeluaran/'.$foto->hashName();
+        $path = public_path('storage/daftar-pengeluaran/' . $foto->hashName());
+
         if($foto != ''){
             Storage::delete('public/daftar-pengeluaran/'.$getImage->foto);
             $foto->storeAs('public/daftar-pengeluaran', $foto->hashName());
+            
+            Gdrive::delete('daftar-pengeluaran/'.$getImage->bukti_pembayaran);
+            Storage::disk('google')->put($namafile, File::get($path));
         }
 
         $validateData['bukti_pembayaran'] = ($foto != '' ? $foto->hashName() : '');
@@ -106,6 +118,8 @@ class DaftarPengeluaranController extends Controller
         if(Storage::exists('public/daftar-pengeluaran/'. $getImage->bukti_pembayaran)){
             Storage::delete('public/daftar-pengeluaran/'. $getImage->bukti_pembayaran);
         }
+
+        Gdrive::delete('daftar-pengeluaran/'.$getImage->bukti_pembayaran);
 
         Helper::logActivity('Hapus daftar pengeluaran dengan id : '.$getImage->id);
 
