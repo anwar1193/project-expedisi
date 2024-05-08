babcd58f687c181003473a31e27c982fbb0f2a69
Heru Anggara <heruanggara0909@gmail.com>
2024-05-08 23:07:00 +0700

    add migration  for changing table jenis pengeluaran

---
+++ database/migrations/2024_05_08_150216_add_column_to_daftar_pengeluarans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('daftar_pengeluarans', function (Blueprint $table) {
+            $table->string('yang_menerima')->after('pengguna_terkait');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('daftar_pengeluarans', function (Blueprint $table) {
+            $table->dropColumn('yang_menerima');
+        });
+    }
+};
