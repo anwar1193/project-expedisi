dabe769df3e65d88a419e45fcc56e85f730ff9f8
Heru Anggara <heruanggara0909@gmail.com>
2024-05-27 15:27:11 +0700

    fix: pemasukkan, update table name in model

--- app/Models/PemasukanLainnya.php
+++ app/Models/PemasukanLainnya.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class PemasukanLainnya extends Model
 {
     use HasFactory;
-    protected $table = 'pemasukan_lainnyas';
+    protected $table = 'pemasukkan_lainnyas';
     protected $primaryKey = 'id';
     protected $guarded = ['id'];
 }
