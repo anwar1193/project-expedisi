2e2f1ae522e5e7eff95ef50b65d44f436e7ecf32
Heru Anggara <heruanggara0909@gmail.com>
2024-05-31 10:58:50 +0700

    feat: invoice, add route for bukti terima pdf

--- storage/framework/views/6b91ba741dd14f3ba32d883057cac13cad5e33b6.php
+++ storage/framework/views/6b91ba741dd14f3ba32d883057cac13cad5e33b6.php
@@ -24,132 +24,6 @@
             margin-bottom: 20px;
         }
 
-        .col-sm-6 {
-            flex: 0 0 48%;
-        }
-
-        .media {
-            display: flex;
-            align-items: center;
-        }
-
-        .card {
-            width: 80%;
-            margin: 20px auto;
-            background: #fff;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            border-radius: 8px;
-        }
-
-        .card-body {
-            padding: 20px;
-        }
-
-        .invo-header, .invo-profile, .invoice-table, .total-section {
-            margin-bottom: 20px;
-        }
-
-        .media {
-            display: flex;
-            align-items: center;
-        }
-
-        .media-left {
-            margin-right: 20px;
-        }
-
-        .img-60 {
-            width: 60px;
-            height: 60px;
-        }
-
-        .media-heading {
-            margin: 0;
-        }
-
-        .digits {
-            font-weight: bold;
-        }
-
-        .invo-header {
-            display: flex;
-            justify-content: space-between;
-        }
-
-        .invo-header .text-md-start {
-            text-align: left;
-        }
-        
-        .invo-header .text-md-end {
-            text-align: right;
-        }
-
-        .text-xs-center {
-            text-align: center;
-        }
-
-        .table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        .table-bordered {
-            border: 1px solid #ddd;
-        }
-
-        .table-bordered th,
-        .table-bordered td {
-            border: 1px solid #ddd;
-            padding: 8px;
-        }
-
-        .table-striped tbody tr:nth-of-type(odd) {
-            background-color: #f9f9f9;
-        }
-
-        .total-section .row {
-            display: flex;
-            justify-content: space-between;
-            margin-bottom: 10px;
-        }
-
-        .card-footer {
-            padding: 10px;
-            background: #f1f1f1;
-            border-top: 1px solid #ddd;
-        }
-
-        .card-footer .btn {
-            background: #007bff;
-            color: #fff;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        .card-footer .btn:hover {
-            background: #0056b3;
-        }
-
-        .total-section {
-            float: right;
-            padding-top: 3px;
-            margin-top: 3px;
-        }
-
-        .total-section .row {
-            padding: 2px;
-        }
-
-        .total-section .col-4 {
-            width: 100%;
-        }
-
-        .total-section .col {
-            width: 100%;
-        }
-
         table {
             width: 100%;
             border-collapse: collapse;
@@ -176,108 +50,97 @@
     <!-- Google font-->
   </head>
   <body>
-    <div class="card">
-        <div class="card-body">
+    <div>
+        <div>
             <div>
                 <table>
                     <tbody>
                         <tr>
-                            <td width="20%" class="table-header">
-                                <h4>Lion Parcel</h4>
-                                <p>
-                                    help@thelionparcel.com<br />
-                                    <span class="digits">+62-21-80820072</span>
-                                </p>
+                            <td class="table-header">
+                                <img src="<?php echo e($picture); ?>" alt="Lion Parcel" style="width: 150px; height: 60px;" />
+                                <h3 style="color: red; padding-right: 10px; font-weight: bold">D Angel Express</h3>
                             </td>
-                            <td></td>
-                            <td class="table-header" style="text-align: end;">
-                                <h3>Invoice #<span class="digits">1069</span></h3>
-                                <p>
-                                    Tanggal : 25 Mei 2024<br />
-                                    Tanggal Jatuh Tempo : 25 Mei 2024
-                                </p>
-                            </td>
+                            <td style="text-align: center; font-weight: bold"><h2>Invoice</h2></td>
+                            <td style="display: flex; align-items: flex-end; padding-top: 45px; text-align: center;"><p>Jakarta, 30 Mei 2024</p></td>
                         </tr>
                     </tbody>
                 </table>
-                
             </div>
-            <div class="invo-profile py-2 my-2">
-                <div class="col-xl-8">
-                    <div class="text-xl-start" id="project">
-                        <div>Tagihan Kepada</div>
-                        <hr>
-                        <div>Nama Customer : Adi Wijaya</div>
-                        <div>Alamat Customer : Samarinda</div>
-                        <div>No. Hp Customer : 085327654839</div>
-                        <div>Email : adi.wijaya@mail.com</div>
-                    </div>
-                </div>
+            <div style="border: 1px solid black; width: 70%">
+                <table style="padding: 10px 0px 0px 10px; font-size: 14px">
+                    <tbody>
+                        <tr>
+                            <td>Invoice No</td>
+                            <td>:</td>
+                            <td>033/INV /LP/IV/2024</td>
+                        </tr>
+                        <tr>
+                            <td>Customer Name</td>
+                            <td>:</td>
+                            <td>PT. Dion Farma Abadi</td>
+                        </tr>
+                        <tr>
+                            <td>Address</td>
+                            <td>:</td>
+                            <td>Jl. Malengkeri Raya</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            <div class="my-3 py-3">
-                <div class="table-responsive invoice-table" id="table">
-                    <table class="table table-bordered table-striped">
+            <div>
+                <p><small>Biaya Pengiriman</small>  PT. Dion Farma Abadi</p>
+            </div>
+            <div>
+                <div>
+                    <table id="data" style="border: 1px solid; width: 100%">
                         <thead>
                             <tr>
-                                <th>Deskripsi</th>
-                                <th>Quantity</th>
-                                <th>Berat</th>
-                                <th>Jumlah</th>
+                                <th style="border: 1px solid">No</th>
+                                <th style="border: 1px solid">No STT</th>
+                                <th style="border: 1px solid">Tanggal</th>
+                                <th style="border: 1px solid">Pengirim</th>
+                                <th style="border: 1px solid">Penerima</th>
+                                <th style="border: 1px solid">Tujuan</th>
+                                <th style="border: 1px solid">Jumlah Pembayaran</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody style="font-size: 14px">
                             <tr>
-                                <td>
-                                    <label>Lorem Ipsum</label>
-                                    <p class="m-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                </td>
-                                <td>5</td>
-                                <td>3 Kg</td>
-                                <td>Rp. 30000</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">1</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">11LP1714021194290</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">2024-04-25 11:59:53 +0000 +0000</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">APOTIK MEDICASTORE/ DION FARMA ABADI</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">DR ROMMY WIJAYA</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">DULOMO SELATAN, KOTA UTARA, GORONTALO</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">62,000</td>
                             </tr>
                             <tr>
-                                <td>
-                                    <label>Lorem Ipsum</label>
-                                    <p class="m-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                </td>
-                                <td>3</td>
-                                <td>2 Kg</td>
-                                <td>Rp. 22500</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">2</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">11LP1714020999725</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">2024-04-25 11:58:24 +0000 +0000</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">APOTIK MEDICASTORE/ DION FARMA ABADI</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">DR ASRIANI</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">DR ASRIANI</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">39,000</td>
                             </tr>
                             <tr>
-                                <td>
-                                    <label>Lorem Ipsum</label>
-                                    <p class="m-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                </td>
-                                <td>10</td>
-                                <td>1.5 Kg</td>
-                                <td>Rp. 20000</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <label>Lorem Ipsum</label>
-                                    <p class="m-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                </td>
-                                <td>10</td>
-                                <td>1 Kg</td>
-                                <td>Rp. 25000</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">3</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">11LP1714020999725</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">2024-04-25 11:59:53 +0000 +0000</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">APOTIK MEDICASTORE/ DION FARMA ABADI</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">dr shinta n barnas</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">POASIA, KENDARI</td>
+                                <td style="border: 1px solid; padding: 5px; text-align: center">67,000</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
-                <div class="total-section float-end pt-3 mt-3">
-                    <div class="row p-2">
-                        <div class="col-4">Sub Total :</div>
-                        <div class="col">Rp. 97500,00</div>
-                    </div>
-                    <div class="row p-2">
-                        <div class="col-4">PPN :</div>
-                        <div class="col">Rp. 9750,00</div>
-                    </div>
-                    <div class="row p-2">
-                        <div class="col-4">Total :</div>
-                        <div class="col">Rp. 117250,00</div>
-                    </div>
+                <div style="display: flex; justify-content: center; text-align: center; line-height: 0.6;">
+                    <p style="text-transform: uppercase">Lion parcel - D Angel Express</p>
+                    <p style="text-transform: capitalize">Jl. Onta Baru no 51, Kelurahan Mandala, Kecamatan Mamajang, Kota Makassar – 90135</p>
+                    <p style="text-transform: capitalize">Sulawesi Selatan</p>
+                    <p style="text-transform: capitalize">Telp : 0411 – 8918311 , 082110071565</p>
+                    <p>Website : http://lionparcel.com/</p>
                 </div>
             </div>
         </div>
