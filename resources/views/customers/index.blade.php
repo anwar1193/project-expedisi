1e336f6cd68b325827d8942e3ac017a9f6a0e76b
Heru Anggara <heruanggara0909@gmail.com>
2024-06-04 15:22:23 +0700

    feat: customer, add button to history limit in view index

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -107,6 +107,9 @@
 																<a class="dropdown-item" href="#" data-bs-toggle="modal" data-original-title="test" data-bs-target="#customerLimitKredit{{ $data->id }}">
 																	<span><i class="pt-2 pe-2" data-feather="tag"></i> Limit Kredit</span>
 																</a>
+																
+																<a class="dropdown-item" href="{{ route('customers.historyLimit', $data->id) }}"><span><i class="pt-2 pe-2" data-feather="list"></i> History Limit</span></a>
+
 															@endif
 
 															<a class="dropdown-item" href="#" data-bs-toggle="modal" data-original-title="test" data-bs-target="#customer{{ $data->id }}">
