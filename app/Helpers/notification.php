9c3a0650d819f96d44be2036108bdea013a6e897
Heru Anggara <heruanggara0909@gmail.com>
2024-06-05 10:26:58 +0700

    edit text hlper notification

--- app/Helpers/notification.php
+++ app/Helpers/notification.php
@@ -7,7 +7,7 @@ if (! function_exists('getNotification')) {
     function getNotification() {
         $jumlah = DataPengiriman::where('status_pembayaran', 2)->count();
         if (Session::get('user_level') == 2) {
-            $text = 'Terdapat '.$jumlah. ' Data Pengiriman Yang Belum Diapprovve';
+            $text = 'Terdapat '.$jumlah. ' Data Pengiriman Yang Belum Diapprove';
         } elseif (Session::get('user_level') == 1) {
             $text = 'Terdapat '.$jumlah. ' Data Pengiriman Yang Masih Berstatus Pending';
         }
