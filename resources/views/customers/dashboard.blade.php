69f3df850c82ef0b8661788e7496236e0f006d7b
Heru Anggara <heruanggara0909@gmail.com>
2024-06-05 20:43:40 +0700

    fix: dashboard customer, add logic for nav-tab

--- resources/views/customers/dashboard.blade.php
+++ resources/views/customers/dashboard.blade.php
@@ -8,6 +8,10 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/datatables.css') }}">
 @endpush
 
+@php
+    $defaultTab = !$resi ? 'home' : 'contact';
+@endphp
+
 @section('content')
 	<div class="container-fluid">
         <div class="row">
@@ -18,19 +22,19 @@
                 <div class="card">
                     <div class="tabbed-card">
                         <div class="card-header pb-0">
-                            @include('customers.component.nav-tabs', ['activeTab' => 'home'])
+                            @include('customers.component.nav-tabs', ['activeTab' => $defaultTab])
                         </div>
 
                         <div class="tab-content" id="top-tabContentsecondary">
-                            <div class="tab-pane fade active show" id="top-homesecondary" role="tabpanel" aria-labelledby="top-home-tab">
+                            <div class="tab-pane fade {{ !$resi ? 'active show' : '' }}" id="top-homesecondary" role="tabpanel" aria-labelledby="top-home-tab">
                                 @include('customers.component.profile', ['data' => $user])
                             </div>
 
                             <div class="tab-pane fade" id="top-profilesecondary" role="tabpanel" aria-labelledby="profile-top-tab">
-                                @include('customers.component.tagihan', ['data' => $tagihan, 'tableId' => 'basic-2'])
+                                @include('customers.component.tagihan', ['data' => $tagihan, 'tableId' => 'basic-1'])
                             </div>
 
-                            <div class="tab-pane fade" id="top-contactsecondary" role="tabpanel" aria-labelledby="contact-top-tab">
+                            <div class="tab-pane fade {{ $resi ? 'active show' : '' }}" id="top-contactsecondary" role="tabpanel" aria-labelledby="contact-top-tab">
                                 @include('customers.component.resi', ['data' => $resi])
                             </div>
 
@@ -65,48 +69,6 @@
 				},
                 searching: false,
 			});
-
-			// $('#basic-2').DataTable({
-			// 	language: {
-			// 		"emptyTable": "Tidak ada data yang tersedia pada tabel ini",
-			// 		"info": "Menampilkan _START_ sampai _END_ dari _TOTAL_ entri",
-			// 		"infoEmpty": "Menampilkan 0 sampai 0 dari 0 entri",
-			// 		"infoFiltered": " (disaring dari _MAX_ entri keseluruhan)",
-			// 		"lengthMenu": "Tampilkan _MENU_ entri",
-			// 		"loadingRecords": "Sedang memuat...",
-			// 		"processing": "Sedang memproses...",
-			// 		"search": "Cari:",
-			// 		"zeroRecords": "Tidak ditemukan data yang sesuai",
-			// 		"paginate": {
-			// 		"first": "Pertama",
-			// 		"last": "Terakhir",
-			// 		"next": "Selanjutnya",
-			// 		"previous": "Sebelumnya"
-			// 		},
-			// 	},
-            //     searching: false,
-			// });
-
-			// $('#basic-3').DataTable({
-			// 	language: {
-			// 		"emptyTable": "Tidak ada data yang tersedia pada tabel ini",
-			// 		"info": "Menampilkan _START_ sampai _END_ dari _TOTAL_ entri",
-			// 		"infoEmpty": "Menampilkan 0 sampai 0 dari 0 entri",
-			// 		"infoFiltered": " (disaring dari _MAX_ entri keseluruhan)",
-			// 		"lengthMenu": "Tampilkan _MENU_ entri",
-			// 		"loadingRecords": "Sedang memuat...",
-			// 		"processing": "Sedang memproses...",
-			// 		"search": "Cari:",
-			// 		"zeroRecords": "Tidak ditemukan data yang sesuai",
-			// 		"paginate": {
-			// 		"first": "Pertama",
-			// 		"last": "Terakhir",
-			// 		"next": "Selanjutnya",
-			// 		"previous": "Sebelumnya"
-			// 		},
-			// 	},
-            //     searching: false,
-			// });
 		})
 	</script>
 	
@@ -125,29 +87,6 @@
 			$('#view-bukti'+id).hide();
 		}
 	</script>
-	{{-- <script>
-        $(document).ready(function() {
-            $('#search-form').on('submit', function(e) {
-                e.preventDefault();
-                var noResi = $('#no_resi').val();
-                $('#resi-result').html('');
-                $.ajax({
-                    url: `{{ route("dashboard.customer") }}`,
-                    method: 'GET',
-                    data: {
-                        no_resi: noResi
-                    },
-                    success: function(response) {
-                        $('#resi-result').html(response);
-                    },
-                    error: function(xhr, status, error) {
-                        console.error('AJAX Error: ' + status + error);
-                        $('#resi-result').html('<div class="alert alert-danger">Error fetching data. Please try again later.</div>');
-                    }
-                });
-            });
-        })
-    </script> --}}
 	@endpush
 
 @endsection
