917aec02fcafb7af9f8ef1014c01fa50e608d8fd
Heru Anggara <heruanggara0909@gmail.com>
2024-06-13 21:55:27 +0700

    update invoice controller

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -8,6 +8,9 @@ use Dompdf\Options;
 use Illuminate\Http\Request;
 use App\Models\Customer;
 use App\Models\DataPengiriman;
+use App\Models\Invoice;
+use App\Models\TransaksiInvoice;
+use Carbon\Carbon;
 
 class InvoiceController extends Controller
 {
@@ -39,37 +42,114 @@ class InvoiceController extends Controller
     public function generateInvoice(Request $request)
     {
         $customer_id = $request->customer;
+        if (!$customer_id) {
+            return back()->with('error', 'Pilih Customer terlebih Dahulu');
+        }
+
+        $customer = Customer::find($customer_id);
+        if (!$customer) {
+            return back()->with('error', 'Customer tidak ditemukan');
+        }
+
+        $data = DataPengiriman::where('kode_customer', $customer->kode_customer)
+                ->where('status_pembayaran', 2)
+                ->orderBy('id', 'DESC')->get();
+        foreach ($data as $item) {
+            $transaksi = TransaksiInvoice::where('data_pengiriman_id', $item->id)
+                    ->orderBy('id', 'ASC')->get();
+            $item->transaksi = $transaksi;
+        }
+
+        if ($data->isEmpty()) {
+            return back()->with('error', 'Customer Belum Memiliki Riwayat Transaksi');
+        }
+
+        $month = Carbon::now()->format('m');
+        $year = Carbon::now()->format('Y');
+        $count_invoice = Invoice::whereMonth('created_at', $month)->count() + 1;
+        $no_invoice = "{$count_invoice}/INV/LP/{$year}";
+
+        $invoice = Invoice::firstOrCreate(
+            ['customer_id' => $customer_id],
+            ['invoice_no' => $no_invoice]
+        );
+
+        $total = DataPengiriman::selectRaw('SUM(ongkir) AS total')
+                ->join('transaksi_invoices', 'transaksi_invoices.data_pengiriman_id', '=', 'data_pengirimen.id')
+                ->where('kode_customer', $customer->kode_customer)->first();
 
         $today = date('Y-m-d');
 
-        $customer = Customer::find($customer_id);
-
-        $data = DataPengiriman::where('kode_customer', $customer->kode_customer)
-                ->orderBy('data_pengirimen.id', 'DESC')->get();
-
-        $total = DataPengiriman::selectRaw('SUM(ongkir) AS total')->where('kode_customer', $customer->kode_customer)->first();
-
-        if (!$customer_id) {
-            return back()->with('error', 'Pilih Customer terlebih Dahulu');
-        } elseif ($data->isEmpty()) {
-            return back()->with('error', 'Customer Belum Memiliki Riwayat Transaksi');
-        }   
-        return view('invoice.hasil', compact('data', 'customer', 'today', 'total'));
+        return view('invoice.hasil', compact('data', 'customer', 'today', 'total', 'invoice'));
     }
 
-    public function generateInvoicePdf($id) 
+    public function all_invoices(Request $request)
     {
-        $customer = Customer::find($id);
+        $tanggal = $request->tanggal;
+        $customer = $request->customer_id;
+        $customers = Customer::all();
+
+        $data = Invoice::select('invoices.invoice_no', 'invoices.created_at', 'customers.id', 'customers.kode_customer', 'customers.nama')
+                ->join('customers', 'customers.id', '=', 'invoices.customer_id')
+                ->when($customer, function ($query, $customer) {
+                    return $query->where('customers.id', $customer);
+                })
+                ->when($tanggal, function ($query, $tanggal) {
+                    return $query->WhereDate('invoices.created_at', $tanggal);
+                })
+                ->orderBy('invoices.id', 'DESC')
+                ->get();
+
+        return view('invoice.all', compact('data', 'customers'));
+    }
+
+    public function handleInvoiceTransactions(Request $request, $id)
+    {
+        $id_pengiriman = $request->id_pengiriman;
+        $diskon = $request->diskon;
+        $customer = Customer::select('customers.id', 'customers.kode_customer', 'customers.nama', 'customers.alamat', 'invoices.invoice_no', 'invoices.id AS invoiceId', 'invoices.diskon')
+                    ->join('invoices', 'invoices.customer_id', '=', 'customers.id')
+                    ->where('customers.id', $id)
+                    ->first();
+
+        if ($id_pengiriman == NULL) {
+            return back()->with('error', 'Belum Ada Data Dipilih');
+        }
+
+        foreach ($id_pengiriman as $pengiriman_id) {
+            $exist = TransaksiInvoice::where('data_pengiriman_id', $pengiriman_id)->exists();
+            if (!$exist) {
+                TransaksiInvoice::create([
+                    'invoice_id' => $customer->invoiceId,
+                    'data_pengiriman_id' => $pengiriman_id
+                ]);
+            }
+        }
+
+        Invoice::find($customer->invoiceId)->update([
+            'diskon' => $diskon
+        ]);
+
+        return redirect()->route('invoice.customer-pdf', ['id' => $customer->id]);
+    }
+
+    public function generateInvoicePdf($id)
+    {
+        $customer = Customer::select('customers.id', 'customers.kode_customer', 'customers.nama', 'customers.alamat', 'invoices.invoice_no', 'invoices.id AS invoiceId', 'invoices.diskon', 'invoices.created_at')
+                    ->join('invoices', 'invoices.customer_id', '=', 'customers.id')
+                    ->where('customers.id', $id)
+                    ->first();
 
         $data = DataPengiriman::where('kode_customer', $customer->kode_customer)
-                ->orderBy('data_pengirimen.id', 'DESC')->get();
-
-        $total = DataPengiriman::selectRaw('SUM(ongkir) AS total')->where('kode_customer', $customer->kode_customer)->first();
+                ->orderBy('id', 'DESC')->get();
+        $total = DataPengiriman::selectRaw('SUM(ongkir) AS total')
+                ->join('transaksi_invoices', 'transaksi_invoices.data_pengiriman_id', '=', 'data_pengirimen.id')
+                ->where('kode_customer', $customer->kode_customer)->first();
 
         $notEmpty = $data->isNotEmpty();
         $waktuCetak = date('d-m-Y H:i:s');
-
         $picture = public_path('assets/lionparcel.png');
+
         $pdf = Pdf::loadView('invoice.hasil-pdf', compact('customer', 'data' ,'picture', 'waktuCetak', 'total', 'notEmpty'));
         return $pdf->stream('Invoice-'.$customer->name.'.pdf');
     }
