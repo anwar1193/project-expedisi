d5ff8b1d107bde587b18cf48879558af12143f8a
Munawar <anwarahmad391@gmail.com>
2024-06-27 09:44:16 +0700

    Update

--- storage/framework/views/7d77f8a436db20905d5743cabb590fef1f6b69ac.php
+++ storage/framework/views/7d77f8a436db20905d5743cabb590fef1f6b69ac.php
@@ -19,9 +19,14 @@
                                         <div class="col-4"><?php echo e($data->tgl_pemasukkan); ?></div>
                                     </div>
                                     <div class="row d-flex py-1 text-start justify-content-start">
-                                        <div class="col-6">Keterangan</div>
+                                        <div class="col-6">Kategori</div>
                                         <div class="col-2">:</div>
-                                        <div class="col-4"><?php echo e($data->keterangan); ?></div>
+                                        <div class="col-4"><?php echo e($data->kategori); ?></div>
+                                    </div>
+                                    <div class="row d-flex py-1 text-start justify-content-start">
+                                        <div class="col-6">Modal</div>
+                                        <div class="col-2">:</div>
+                                        <div class="col-4"><?php echo e($data->modal); ?></div>
                                     </div>
                                     <div class="row d-flex py-1 text-start justify-content-start">
                                         <div class="col-6">Bukti Pembayaran</div>
