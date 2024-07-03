ed1d20f4f3335f8b5adcc0ca5b3afadfd7c15040
Heru Anggara <heruanggara0909@gmail.com>
2024-07-03 10:51:57 +0700

    feat: add feature mail invoice

---
+++ app/Mail/Invoice.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Invoice as ModelsInvoice;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Attachment;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class Invoice extends Mailable
+{
+    use Queueable, SerializesModels;
+
+     /**
+     * The order instance.
+     *
+     * @var \App\Models\Invoice
+     */
+    public $invoice;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($invoice)
+    {
+        $this->invoice = $invoice;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Invoice - Lion Parcel',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'email.invoicemail',
+            with: [
+                'invoice_no' => $this->invoice->invoice_no,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return Attachment::fromStorage('public/invoices/Invoice-Ametconsecteturea.pdf');
+    }
+}
