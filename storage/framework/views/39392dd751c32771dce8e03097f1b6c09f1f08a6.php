381f2f7df7385642d9179426e6f4adf9d01993fa
Munawar <anwarahmad391@gmail.com>
2024-07-11 21:01:36 +0700

    update 11 jul 2024

---
+++ storage/framework/views/39392dd751c32771dce8e03097f1b6c09f1f08a6.php
@@ -0,0 +1,41 @@
+<div class="modal fade modal-bookmark" id="modalMerchandise<?php echo e($data->id); ?>" tabindex="-1" role="dialog"
+    aria-labelledby="modalMerchandiseLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content justify-content-start">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Detail Merchandise</h5>
+                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="col-xl-12 xl-20 col-md-7 box-col-12">
+                    <div class="tab-content" id="v-pills-tabContent">
+                        <div class="tab-pane contact-tab-0 tab-content-child fade show active" id="v-pills-user"
+                            role="tabpanel" aria-labelledby="v-pills-user-tab">
+                            <div class="profile-mail">
+                                <div class="email-general mb-2">
+                                    <div class="row d-flex py-1 text-start justify-content-start">
+                                        <div class="col-6">Nama Merchandise</div>
+                                        <div class="col-2">:</div>
+                                        <div class="col-4 text-capitalize"><?php echo e($data->nama); ?></div>
+                                    </div>
+                                    <div class="row d-flex py-1 text-start justify-content-start">
+                                        <div class="col-6">Nilai</div>
+                                        <div class="col-2">:</div>
+                                        <div class="col-4"><?php echo e(number_format($data->nilai, 0, '.', ',')); ?></div>
+                                    </div>
+                                    <div class="row d-flex py-1 text-start justify-content-start">
+                                        <div class="col-6">Gambar</div>
+                                        <div class="col-2">:</div>
+                                        <div class="col-4">
+                                            <img src="<?php echo e(asset('storage/merchandise/'.$data->gambar)); ?>" alt="" width="200px" class="img-fluid mt-2">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><?php /**PATH /Users/munawarahmad/Documents/Applications/projectku/project-expedisi/resources/views/merchandise/detail.blade.php ENDPATH**/ ?>
