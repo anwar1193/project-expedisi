50ad4eb0841539b95603190f9a41def527af135c
Heru Anggara <heruanggara0909@gmail.com>
2024-07-12 16:56:28 +0700

    style: posisi cash view, styling card

--- resources/views/posisi-cash/index.blade.php
+++ resources/views/posisi-cash/index.blade.php
@@ -50,31 +50,47 @@
 							</div>
 						@endif
 
-                        <div class="row ps-3 ms-3 py-3 my-3">
+                        <div class="row ps-3 ms-3 pt-3 mt-3">
                             <div class="col-xl-4 col-md-4 col-sm-4 box-col-4 des-xl-25 rate-sec">
                                 <div class="card shadow-lg" style="width: 18rem; border-radius:15px; background-color: rgb(219, 176, 55)">
-                                    <div class="card-body text-center fw-bold">
-                                      <h5 class="card-title pb-3 text-center fw-bold">Total Saldo</h5>
+                                    <div class="card-body fw-bold">
+                                        <div class="row">
+                                            <div class="col-4 d-flex align-items-center"><h1><i class="icofont icofont-sale-discount"></i></h1></div>
+                                            <div class="col">
+                                                <div class="row"><h5 class="fw-bold">Total Saldo</h5></div>
+                                                <div class="row"><h5 class="fw-bold">{{ 'Rp '.number_format($saldo, 0, '.', '.') }}</h5></div>
+                                            </div>
+                                        </div>
+
+                                      {{-- <h5 class="card-title pb-3 text-center fw-bold">Total Saldo</h5>
                                       <h4><i class="icofont icofont-sale-discount"></i></h4>
-                                      <h3 class="fw-bold">{{ 'Rp '.number_format($saldo, 0, '.', '.') }}</h3>
+                                      <h3 class="fw-bold">{{ 'Rp '.number_format($saldo, 0, '.', '.') }}</h3> --}}
                                     </div>
                                   </div>
                               </div>
                               <div class="col-xl-4 col-md-4 col-sm-4 box-col-4 des-xl-25 rate-sec">
                                 <div class="card shadow-lg" style="width: 18rem; border-radius:15px; background-color: rgb(33, 174, 47)">
-                                    <div class="card-body text-center fw-bold">
-                                        <h5 class="card-title pb-3 text-center fw-bold">Pemasukan</h5>
-                                        <h4><i class="icofont icofont-arrow-up"></i></h4>
-                                        <h3 class="fw-bold">{{ 'Rp '.number_format($pemasukan->total ?? 0, 0, '.', '.') }}</h3>
-                                      </div>
-                                  </div>
+                                    <div class="card-body fw-bold">
+                                        <div class="row">
+                                            <div class="col-4 d-flex align-items-center"><h1><i class="icofont icofont-arrow-up"></i></h1></div>
+                                            <div class="col">
+                                                <div class="row"><h5 class="fw-bold">Pemasukkan</h5></div>
+                                                <div class="row"><h5 class="fw-bold">{{ 'Rp '.number_format($pemasukan->total ?? 0, 0, '.', '.') }}</h5></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                               </div>
                               <div class="col-xl-4 col-md-4 col-sm-4 box-col-4 des-xl-25 rate-sec">
                                 <div class="card shadow-lg" style="width: 18rem; border-radius:15px; background-color: rgb(200, 75, 75)">
                                     <div class="card-body text-center fw-bold">
-                                        <h5 class="card-title pb-3 text-center fw-bold">Pengeluaran</h5>
-                                        <h4><i class="icofont icofont-arrow-down"></i></h4>
-                                        <h3 class="fw-bold">{{ 'Rp '.number_format($pengeluaran->total ?? 0, 0, '.', '.') }}</h3>
+                                        <div class="row">
+                                            <div class="col-4 d-flex align-items-center"><h1><i class="icofont icofont-arrow-down"></i></h1></div>
+                                            <div class="col">
+                                                <div class="row"><h5 class="fw-bold">Pengeluaran</h5></div>
+                                                <div class="row"><h5 class="fw-bold">{{ 'Rp '.number_format($pengeluaran->total ?? 0, 0, '.', '.') }}</h5></div>
+                                            </div>
+                                        </div>
                                       </div>
                                   </div>
                               </div>
