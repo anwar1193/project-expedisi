3ad61cb2d388db42a366bc103ca17e01a84f3c58
Munawar <anwarahmad391@gmail.com>
2024-07-12 11:12:02 +0700

    update

--- storage/framework/views/117493e34793597a3eb6641ada5051638c0d864c.php
+++ storage/framework/views/117493e34793597a3eb6641ada5051638c0d864c.php
@@ -108,7 +108,7 @@ $message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>" type="file" width="48" height="48" name="gambar" />
-                                        <div id="nominal" class="form-text text-danger">*Maximun Size 2 Mb</div>
+                                        <div id="nominal" class="form-text text-danger">*Maximun Size 10 Mb</div>
 
 										<?php $__errorArgs = ['gambar'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
