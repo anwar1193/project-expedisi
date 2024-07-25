62cc1e9058d174ef156808865f35e56faeb046e3
Munawar <anwarahmad391@gmail.com>
2024-07-25 09:39:51 +0700

    update

--- storage/framework/views/b79a0435da07e0d4637d320d95cb6d946a6bec13.php
+++ storage/framework/views/b79a0435da07e0d4637d320d95cb6d946a6bec13.php
@@ -19,6 +19,19 @@
 	<?php echo $__env->renderComponent(); ?>
 	
 	<div class="container-fluid">
+		<form class="d-flex flex-column col-12 mb-2" role="search" action="" method="GET">
+			<div class="d-flex justify-content-end">
+                <div id="customer_id" class="px-2">
+                    <input class="form-control" type="date" name="tanggal" value="<?php echo e($tanggal); ?>" />
+                </div>
+				<div class="px-1">
+					<button type="submit" class="btn btn-primary" title="Cari"><i class="fa fa-search"></i> Cari</button>
+				</div>
+				<div class="px-1">
+					<a href="<?php echo e(route('posisi-cash')); ?>" class="btn btn-md btn-secondary" title="Reset"><i class="fa fa-refresh"></i> Reset</a>
+				</div>
+			</div>
+		</form>
         <div class="row">
         </div>
 	    <div class="row">
@@ -54,51 +67,65 @@
 						<?php endif; ?>
 
                         <div class="row ps-3 ms-3 pt-3 mt-3">
-                            <div class="col-xl-4 col-md-4 col-sm-4 box-col-4 des-xl-25 rate-sec">
-                                <div class="card shadow-lg" style="width: 18rem; border-radius:15px; background-color: rgb(219, 176, 55)">
-                                    <div class="card-body fw-bold">
-                                        <div class="row">
-                                            <div class="col-4 d-flex align-items-center"><h1><i class="icofont icofont-sale-discount"></i></h1></div>
-                                            <div class="col">
-                                                <div class="row"><h5 class="fw-bold">Total Saldo</h5></div>
-                                                <div class="row"><h5 class="fw-bold"><?php echo e('Rp '.number_format($saldo, 0, '.', '.')); ?></h5></div>
-                                            </div>
-                                        </div>
+							<div class="col-xl-4 col-md-4 col-sm-4 box-col-4 des-xl-25 rate-sec">
+								<a class="text-white" href="<?php echo e(route('posisi-cash.history-saldo')); ?>">
+									<div class="card shadow-lg" style="width: 18rem; border-radius:15px; background-color: rgb(219, 176, 55)">
+										<div class="card-body fw-bold">
+											<div class="row">
+												<div class="col-4 d-flex align-items-center"><h1><i class="icofont icofont-sale-discount"></i></h1></div>
+												<div class="col">
+													<div class="row"><h5 class="fw-bold">Total Saldo</h5></div>
+													<div class="row"><h5 class="fw-bold"><?php echo e('Rp '.number_format($saldoToday->saldo, 0, '.', '.')); ?></h5></div>
+												</div>
+											</div>
 
-                                      
-                                    </div>
-                                  </div>
+										
+										</div>
+									</div>
+								</a>
                               </div>
                               <div class="col-xl-4 col-md-4 col-sm-4 box-col-4 des-xl-25 rate-sec">
-                                <div class="card shadow-lg" style="width: 18rem; border-radius:15px; background-color: rgb(33, 174, 47)">
-                                    <div class="card-body fw-bold">
-                                        <div class="row">
-                                            <div class="col-4 d-flex align-items-center"><h1><i class="icofont icofont-arrow-up"></i></h1></div>
-                                            <div class="col">
-                                                <div class="row"><h5 class="fw-bold">Pemasukkan</h5></div>
-                                                <div class="row"><h5 class="fw-bold"><?php echo e('Rp '.number_format($pemasukan->total ?? 0, 0, '.', '.')); ?></h5></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+								<a class="text-white"  href="<?php echo e(route('posisi-cash.history-pemasukan')); ?>">
+									<div class="card shadow-lg" style="width: 18rem; border-radius:15px; background-color: rgb(33, 174, 47)">
+										<div class="card-body fw-bold">
+											<div class="row">
+												<div class="col-4 d-flex align-items-center"><h1><i class="icofont icofont-arrow-up"></i></h1></div>
+												<div class="col">
+													<div class="row"><h5 class="fw-bold">Pemasukkan</h5></div>
+													<div class="row"><h5 class="fw-bold"><?php echo e('Rp '.number_format($pemasukan->total ?? 0, 0, '.', '.')); ?></h5></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</a>
                               </div>
                               <div class="col-xl-4 col-md-4 col-sm-4 box-col-4 des-xl-25 rate-sec">
-                                <div class="card shadow-lg" style="width: 18rem; border-radius:15px; background-color: rgb(200, 75, 75)">
-                                    <div class="card-body text-center fw-bold">
-                                        <div class="row">
-                                            <div class="col-4 d-flex align-items-center"><h1><i class="icofont icofont-arrow-down"></i></h1></div>
-                                            <div class="col">
-                                                <div class="row"><h5 class="fw-bold">Pengeluaran</h5></div>
-                                                <div class="row"><h5 class="fw-bold"><?php echo e('Rp '.number_format($pengeluaran->total ?? 0, 0, '.', '.')); ?></h5></div>
-                                            </div>
-                                        </div>
-                                      </div>
-                                  </div>
+								<a class="text-white"  href="<?php echo e(route('posisi-cash.history-pengeluaran')); ?>">
+									<div class="card shadow-lg" style="width: 18rem; border-radius:15px; background-color: rgb(200, 75, 75)">
+										<div class="card-body text-center fw-bold">
+											<div class="row">
+												<div class="col-4 d-flex align-items-center"><h1><i class="icofont icofont-arrow-down"></i></h1></div>
+												<div class="col">
+													<div class="row"><h5 class="fw-bold">Pengeluaran</h5></div>
+													<div class="row"><h5 class="fw-bold"><?php echo e('Rp '.number_format($pengeluaran->total ?? 0, 0, '.', '.')); ?></h5></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</a>
                               </div>
                         </div>
 	                </div>
                     <div class="card-footer">
                         <div class="d-flex justify-content-evenly">
+							<?php if($tanggal == date('Y-m-d')): ?>
+								<form action="<?php echo e(route('posisi-cash.closing')); ?>" method="POST">
+									<?php echo csrf_field(); ?>
+									<input type="hidden" name="saldo" value="<?php echo e($saldo); ?>">
+									<input type="hidden" name="tanggal" value="<?php echo e($tanggal); ?>">
+									<button class="btn btn-secondary" type="submit" onclick="return confirm('Apakah Anda Yakin?')">Closing</button>
+								</form>
+							<?php endif; ?>
                             <button class="btn btn-secondary" href="#" data-bs-toggle="modal" data-original-title="test" data-bs-target="#pemasukan">Input Pemasukan</button>
                             <button class="btn btn-secondary" href="#" data-bs-toggle="modal" data-original-title="test" data-bs-target="#pengeluaran">Input Pengeluaran</button>
                         </div>
